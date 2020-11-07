73bc28dca78dd926b01dcfee418926ecec1bc2cf
ethan <netocrat666gmail.com>
2020-11-07 13:42:27 +0300

    ...

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const
-  socks5 = require('simple-socks'),
-  server = socks5.createServer().listen(process.env.PORT || 80, function () {
-  console.log('SOCKS5 proxy server started');
+const fs = require('fs');
+const socks5 = require('simple-socks')
+
+const [ user_name, pass ] = fs.readFileSync("./login", "utf8").match(/^.+/gm);
+if (!pass) {
+    console.error('Incorrect login data');
+    process.exit(1)
+}
+
+const server = socks5.createServer({
+		authenticate : function (username, password, socket, callback) {
+			if (username !== user_name || password !== pass) {
+				return setImmediate(callback, new Error('invalid credentials'));
+			}
+
+			return setImmediate(callback);
+		}
+	}).listen(process.env.PORT || 8000, () => {
+		let addr = server.address();
+		console.log(`SOCKS5 proxy server started on ${typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port}`);
 });
   
  
---
+++ test.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const fs = require('fs');
+
+/*
+linux:
+openssl genrsa -out client-key.pem 2048
+openssl req -new -key client-key.pem -out client.csr
+openssl x509 -req -in client.csr -signkey client-key.pem -out client-cert.pem
+*/
+
+
+const options = {
+  key: fs.readFileSync('client-key.pem'),
+  cert: fs.readFileSync('client-cert.pem')
+};
+/*
+const options = {
+  pfx: fs.readFileSync('cert.pfx'),
+  passphrase: 'test_cert'
+};
+*/
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(443);
